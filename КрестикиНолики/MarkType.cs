3031e42c6f143f47cb82dc95f5653413143eca52
Roman Levchenkov <rlevch@gmail.com>
2018-08-09 18:30:43 +0300

    Add project files.

---
+++ КрестикиНолики/MarkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КрестикиНолики
+{
+    /// <summary>
+    /// The type of value a cell in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is an O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is a X
+        /// </summary>
+        Cross
+    }
+}
